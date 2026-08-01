d6b3c25400931926d1bb32806247c231d66d81b9
chickymunch10 <simple1x12@gmail.com>
2026-08-01 19:18:51 +0500

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1,13 +1,10 @@
 const menu = [
+
+/* ================= BURGERS ================= */
+
 {
 name:"Zinger Burger",
 price:375,
-category:"Burgers"
-},
-
-{
-name:"Grill Patty Burger",
-price:320,
 category:"Burgers"
 },
 
@@ -18,8 +15,14 @@ category:"Burgers"
 },
 
 {
-name:"Beef Burger",
-price:475,
+name:"Zinger Jumbo Burger",
+price:675,
+category:"Burgers"
+},
+
+{
+name:"Grill Patty Burger",
+price:320,
 category:"Burgers"
 },
 
@@ -29,181 +32,204 @@ price:695,
 category:"Burgers"
 },
 
-{
-name:"Zinger Jumbo Burger",
-price:675,
-category:"Burgers"
-},
 
-{
-name:"Chicken Burger",
-price:299,
-category:"Burgers"
-},
-
-{
-name:"Chicken Cheese Burger",
-price:399,
-category:"Burgers"
-},
-
-{
-name:"Mexican Sandwich",
-price:750,
-category:"Sandwich"
-},
-
-{
-name:"Euro Sandwich",
-price:750,
-category:"Sandwich"
-},
-
-{
-name:"Alfredo Pasta",
-price:495,
-category:"Pasta"
-},
-
-{
-name:"Crunchy Pasta",
-price:495,
-category:"Pasta"
-},{
-name:"Chicken Tikka Small",
-price:500,
-category:"Pizza"
-},
-
-{
-name:"Chicken Fajita Small",
-price:500,
-category:"Pizza"
-},
-
-{
-name:"Chicken Tandoori Small",
-price:500,
-category:"Pizza"
-},
+/* ================= CHICKY LOCAL PIZZA ================= */
 
 {
 name:"BBQ Chicken Small",
 price:500,
-category:"Pizza"
-},
-
-{
-name:"Hot N Spicy Small",
-price:500,
-category:"Pizza"
-},
-
-{
-name:"Chicken Tikka Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Chicken Fajita Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Chicken Tandoori Medium",
-price:1350,
-category:"Pizza"
+category:"Chicky Local Pizza"
 },
 
 {
 name:"BBQ Chicken Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Hot N Spicy Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Chicky Extreme Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Munch Special Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Behari Kebab Medium",
-price:1350,
-category:"Pizza"
-},
-
-{
-name:"Chicken Tikka Large",
-price:1950,
-category:"Pizza"
-},
-
-{
-name:"Chicken Fajita Large",
-price:1950,
-category:"Pizza"
-},
-
-{
-name:"Chicken Tandoori Large",
-price:1950,
-category:"Pizza"
+price:1150,
+category:"Chicky Local Pizza"
 },
 
 {
 name:"BBQ Chicken Large",
-price:1950,
-category:"Pizza"
+price:1350,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"BBQ Chicken Party",
+price:2200,
+category:"Chicky Local Pizza"
+},
+
+
+{
+name:"Chicken Fajita Small",
+price:500,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Fajita Medium",
+price:1150,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Fajita Large",
+price:1350,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Fajita Party",
+price:2200,
+category:"Chicky Local Pizza"
+},
+
+
+{
+name:"Chicken Tikka Small",
+price:500,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Tikka Medium",
+price:1150,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Tikka Large",
+price:1350,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Tikka Party",
+price:2200,
+category:"Chicky Local Pizza"
+},
+
+
+{
+name:"Chicken Tandoori Small",
+price:500,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Tandoori Medium",
+price:1150,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Tandoori Large",
+price:1350,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Chicken Tandoori Party",
+price:2200,
+category:"Chicky Local Pizza"
+},
+
+
+{
+name:"Hot N Spicy Small",
+price:500,
+category:"Chicky Local Pizza"
+},
+
+{
+name:"Hot N Spicy Medium",
+price:1150,
+category:"Chicky Local Pizza"
 },
 
 {
 name:"Hot N Spicy Large",
-price:1950,
-category:"Pizza"
+price:1350,
+category:"Chicky Local Pizza"
 },
 
 {
-name:"Chicky Extreme Large",
-price:1950,
-category:"Pizza"
+name:"Hot N Spicy Party",
+price:2200,
+category:"Chicky Local Pizza"
+},
+
+
+/* ================= PASTA ================= */
+
+{
+name:"Alfredo Pasta Small",
+price:495,
+category:"Pasta"
 },
 
 {
-name:"Munch Special Large",
-price:1950,
-category:"Pizza"
+name:"Alfredo Pasta Large",
+price:750,
+category:"Pasta"
 },
 
 {
-name:"Behari Kebab Large",
-price:1950,
-category:"Pizza"
-},{
+name:"Crunchy Pasta Small",
+price:495,
+category:"Pasta"
+},
+
+{
+name:"Crunchy Pasta Large",
+price:750,
+category:"Pasta"
+},
+
+
+/* ================= PLATTERS ================= */
+
+{
 name:"Special Roasted Platter",
-price:999,
+price:850,
 category:"Platters"
 },
 
 {
 name:"Classic Roll Platter",
-price:1499,
+price:1050,
 category:"Platters"
 },
+
+
+/* ================= ZINGER DEALS ================= */
+
+{
+name:"Deal 1 - 1 Zinger Fries & Drink",
+price:550,
+category:"Zinger Deals"
+},
+
+{
+name:"Deal 2 - 2 Zinger Fries & Half Ltr Drink",
+price:1050,
+category:"Zinger Deals"
+},
+
+{
+name:"Deal 3 - 3 Zinger 4 Wings & 1 Ltr Drink",
+price:1550,
+category:"Zinger Deals"
+},
+
+{
+name:"Deal 4 - 5 Zinger Fries & 1 Ltr Drink",
+price:2050,
+category:"Zinger Deals"
+},
+
+
+/* ================= ADD ONS ================= */
 
 {
 name:"Loaded Fries Small",
@@ -218,25 +244,25 @@ category:"Add Ons"
 },
 
 {
+name:"Chicken Pieces",
+price:250,
+category:"Add Ons"
+},
+
+{
+name:"Hot Wings",
+price:400,
+category:"Add Ons"
+},
+
+{
 name:"Fries Small",
 price:150,
 category:"Add Ons"
 },
 
 {
-name:"Fries Medium",
-price:200,
-category:"Add Ons"
-},
-
-{
 name:"Fries Large",
-price:250,
-category:"Add Ons"
-},
-
-{
-name:"Chicken Piece",
 price:250,
 category:"Add Ons"
 },
@@ -248,74 +274,131 @@ category:"Add Ons"
 },
 
 {
-name:"Hot Wings Bucket",
-price:400,
-category:"Add Ons"
-},
-
-{
-name:"Oven Baked Wings Bucket",
-price:450,
-category:"Add Ons"
-},
-
-{
-name:"Nuggets Bucket",
+name:"Nuggets",
 price:350,
 category:"Add Ons"
 },
 
+
+/* ================= WRAPS & ROLLS ================= */
+
 {
-name:"2 Zinger Burger Deal",
-price:1199,
-category:"Deals"
+name:"Zinger Strip Wrap Small",
+price:450,
+category:"Wraps & Rolls"
 },
 
 {
-name:"Medium Pizza Deal",
-price:1849,
-category:"Deals"
+name:"Zinger Strip Wrap Large",
+price:650,
+category:"Wraps & Rolls"
 },
 
 {
-name:"Medium Crown Crust Pizza Deal",
-price:1499,
-category:"Deals"
+name:"BBQ Roll Paratha",
+price:350,
+category:"Wraps & Rolls"
 },
 
 {
-name:"2 Medium Pizza Deal",
-price:2750,
-category:"Deals"
+name:"Chicken Wrap Small",
+price:450,
+category:"Wraps & Rolls"
 },
 
 {
-name:"Large Stuff Crust Pizza Deal",
-price:2899,
-category:"Deals"
+name:"Chicken Wrap Large",
+price:650,
+category:"Wraps & Rolls"
 },
 
 {
-name:"Pepsi 345ml",
-price:120,
-category:"Drinks"
+name:"Zinger Roll Paratha",
+price:400,
+category:"Wraps & Rolls"
 },
 
 {
-name:"Pepsi 500ml",
-price:180,
-category:"Drinks"
+name:"Zinger Cheese Roll Paratha",
+price:450,
+category:"Wraps & Rolls"
 },
 
 {
-name:"1 Liter Drink",
+name:"Pizza Paratha",
+price:499,
+category:"Wraps & Rolls"
+},
+
+{
+name:"Chicken Shawarma",
 price:250,
+category:"Wraps & Rolls"
+},
+
+{
+name:"Zinger Shawarma",
+price:300,
+category:"Wraps & Rolls"
+},
+
+
+/* ================= STARTER + SANDWICH ================= */
+
+{
+name:"Calzone Chunks",
+price:950,
+category:"Special Starter + Sandwich"
+},
+
+{
+name:"Oven Baked Wing",
+price:450,
+category:"Special Starter + Sandwich"
+},
+
+{
+name:"Pizza Stacker",
+price:850,
+category:"Special Starter + Sandwich"
+},
+
+{
+name:"Mexican Sandwich",
+price:750,
+category:"Special Starter + Sandwich"
+},
+
+{
+name:"Euro Sandwich",
+price:750,
+category:"Special Starter + Sandwich"
+},
+
+
+/* ================= DRINKS ================= */
+
+{
+name:"Regular Drink 345ml (Cola Next / Freezup)",
+price:100,
 category:"Drinks"
 },
 
 {
-name:"1.5 Liter Drink",
-price:320,
+name:"Half Liter Drink 500ml (Cola Next / Freezup)",
+price:130,
+category:"Drinks"
+},
+
+{
+name:"1 Liter Drink (Cola Next / Freezup)",
+price:200,
+category:"Drinks"
+},
+
+{
+name:"1.5 Liter Drink (Cola Next / Freezup)",
+price:250,
 category:"Drinks"
 }
 
