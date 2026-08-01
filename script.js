1b0cd3b7425bee2edf26627820b13e81223c3e14
chickymunch10 <simple1x12@gmail.com>
2026-08-01 18:14:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,90 +5,68 @@ const total = document.getElementById("total");
 let cart = [];
 
 function renderProducts() {
-  products.innerHTML = "";
+    products.innerHTML = "";
 
-  menu.forEach((item) => {
-    products.innerHTML += `
-      <div class="card">
-        <h3>${item.name}</h3>
-        <p class="price">Rs ${item.price}</p>
-        <button onclick="addToCart('${item.name}')">
-          Add to Cart
-        </button>
-      </div>
-    `;
-  });
+    menu.forEach((item, index) => {
+        products.innerHTML += `
+        <div class="card">
+            <h3>${item.name}</h3>
+            <p>Rs ${item.price}</p>
+            <button onclick="addToCart(${index})">
+                Add To Cart
+            </button>
+        </div>`;
+    });
 }
 
-function addToCart(name) {
-  const item = menu.find(p => p.name === name);
-
-  cart.push(item);
-
-  renderCart();
+function addToCart(index) {
+    cart.push(menu[index]);
+    renderCart();
 }
-
 function renderCart() {
+    cartItems.innerHTML = "";
 
-  cartItems.innerHTML = "";
+    let sum = 0;
 
-  let sum = 0;
+    cart.forEach((item) => {
+        sum += item.price;
 
-  cart.forEach(item => {
+        cartItems.innerHTML += `
+        <div>
+            ${item.name} - Rs ${item.price}
+        </div>`;
+    });
 
-    sum += item.price;
-
-    cartItems.innerHTML += `
-      <p>${item.name} - Rs ${item.price}</p>
-    `;
-
-  });
-
-  total.innerText = sum;
-
+    total.innerText = sum;
 }
 
-document.getElementById("order").onclick = function(){
+document.getElementById("order").onclick = function () {
 
-const customer =
-document.getElementById("customer").value;
+    const customer = document.getElementById("customer").value;
+    const phone = document.getElementById("phone").value;
+    const address = document.getElementById("address").value;
 
-const phone =
-document.getElementById("phone").value;
-
-const address =
-document.getElementById("address").value;
-
-let message =
-`🍗 Chicky Munch Order
+    let message = `🍔 Chicky Munch Order
 
 Name: ${customer}
-
 Phone: ${phone}
-
 Address: ${address}
 
 ------------------
 
 `;
-
-cart.forEach(item=>{
-
-message+=`${item.name} - Rs ${item.price}
-`;
-
+cart.forEach((item) => {
+    message += `${item.name} - Rs ${item.price}\n`;
 });
 
 message += `
-
 ------------------
-
-Total = Rs ${total.innerText}`;
+Total = Rs ${total.innerText}
+`;
 
 window.open(
-
-`https://wa.me/923331917184?text=${encodeURIComponent(message)}`
-
+    `https://wa.me/923331917184?text=${encodeURIComponent(message)}`,
+    "_blank"
 );
 
 };
