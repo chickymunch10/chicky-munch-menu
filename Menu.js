67c42d11cde97b1a2620f2fe4d437161b218798f
chickymunch10 <simple1x12@gmail.com>
2026-08-01 18:05:02 +0500

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,322 @@
+const menu = [
+{
+name:"Zinger Burger",
+price:375,
+category:"Burgers"
+},
+
+{
+name:"Grill Patty Burger",
+price:320,
+category:"Burgers"
+},
+
+{
+name:"Zinger Cheese Burger",
+price:475,
+category:"Burgers"
+},
+
+{
+name:"Beef Burger",
+price:475,
+category:"Burgers"
+},
+
+{
+name:"CM Special Burger",
+price:695,
+category:"Burgers"
+},
+
+{
+name:"Zinger Jumbo Burger",
+price:675,
+category:"Burgers"
+},
+
+{
+name:"Chicken Burger",
+price:299,
+category:"Burgers"
+},
+
+{
+name:"Chicken Cheese Burger",
+price:399,
+category:"Burgers"
+},
+
+{
+name:"Mexican Sandwich",
+price:750,
+category:"Sandwich"
+},
+
+{
+name:"Euro Sandwich",
+price:750,
+category:"Sandwich"
+},
+
+{
+name:"Alfredo Pasta",
+price:495,
+category:"Pasta"
+},
+
+{
+name:"Crunchy Pasta",
+price:495,
+category:"Pasta"
+},{
+name:"Chicken Tikka Small",
+price:500,
+category:"Pizza"
+},
+
+{
+name:"Chicken Fajita Small",
+price:500,
+category:"Pizza"
+},
+
+{
+name:"Chicken Tandoori Small",
+price:500,
+category:"Pizza"
+},
+
+{
+name:"BBQ Chicken Small",
+price:500,
+category:"Pizza"
+},
+
+{
+name:"Hot N Spicy Small",
+price:500,
+category:"Pizza"
+},
+
+{
+name:"Chicken Tikka Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Chicken Fajita Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Chicken Tandoori Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"BBQ Chicken Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Hot N Spicy Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Chicky Extreme Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Munch Special Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Behari Kebab Medium",
+price:1350,
+category:"Pizza"
+},
+
+{
+name:"Chicken Tikka Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"Chicken Fajita Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"Chicken Tandoori Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"BBQ Chicken Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"Hot N Spicy Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"Chicky Extreme Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"Munch Special Large",
+price:1950,
+category:"Pizza"
+},
+
+{
+name:"Behari Kebab Large",
+price:1950,
+category:"Pizza"
+},{
+name:"Special Roasted Platter",
+price:999,
+category:"Platters"
+},
+
+{
+name:"Classic Roll Platter",
+price:1499,
+category:"Platters"
+},
+
+{
+name:"Loaded Fries Small",
+price:495,
+category:"Add Ons"
+},
+
+{
+name:"Loaded Fries Large",
+price:750,
+category:"Add Ons"
+},
+
+{
+name:"Fries Small",
+price:150,
+category:"Add Ons"
+},
+
+{
+name:"Fries Medium",
+price:200,
+category:"Add Ons"
+},
+
+{
+name:"Fries Large",
+price:250,
+category:"Add Ons"
+},
+
+{
+name:"Chicken Piece",
+price:250,
+category:"Add Ons"
+},
+
+{
+name:"Chicken Bucket",
+price:600,
+category:"Add Ons"
+},
+
+{
+name:"Hot Wings Bucket",
+price:400,
+category:"Add Ons"
+},
+
+{
+name:"Oven Baked Wings Bucket",
+price:450,
+category:"Add Ons"
+},
+
+{
+name:"Nuggets Bucket",
+price:350,
+category:"Add Ons"
+},
+
+{
+name:"2 Zinger Burger Deal",
+price:1199,
+category:"Deals"
+},
+
+{
+name:"Medium Pizza Deal",
+price:1849,
+category:"Deals"
+},
+
+{
+name:"Medium Crown Crust Pizza Deal",
+price:1499,
+category:"Deals"
+},
+
+{
+name:"2 Medium Pizza Deal",
+price:2750,
+category:"Deals"
+},
+
+{
+name:"Large Stuff Crust Pizza Deal",
+price:2899,
+category:"Deals"
+},
+
+{
+name:"Pepsi 345ml",
+price:120,
+category:"Drinks"
+},
+
+{
+name:"Pepsi 500ml",
+price:180,
+category:"Drinks"
+},
+
+{
+name:"1 Liter Drink",
+price:250,
+category:"Drinks"
+},
+
+{
+name:"1.5 Liter Drink",
+price:320,
+category:"Drinks"
+}
+
+];
